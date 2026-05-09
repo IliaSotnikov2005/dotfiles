3217c21b2203b7a7c18e09442da5b7ad8605142c
Ilya Sotnikov <ilia12st@yandex.ru>
2026-05-09 11:25:03 +0300

    fix nvim bufferline

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -30,6 +30,34 @@ return {
 				},
 				-- Автоматическая фильтрация через scope.nvim
 				groups = { items = {}, options = { fill_hl = "TabLineFill" } },
+
+				custom_filter = function(buf_number)
+					local bufname = vim.api.nvim_buf_get_name(buf_number)
+					local buftype = vim.bo[buf_number].buftype
+
+					-- Только реальные файлы (не терминал, не help, не quickfix)
+					if buftype ~= "" then
+						return false
+					end
+					-- Исключаем буферы без имени (например, [No Name])
+					if bufname == "" then
+						return false
+					end
+					-- Убираем dashboard, lazy и т.д.
+					local exclude_ft = {
+						"alpha",
+						"dashboard",
+						"lazy",
+						"mason",
+						"help",
+						"qf",
+					}
+					if vim.tbl_contains(exclude_ft, vim.bo[buf_number].filetype) then
+						return false
+					end
+					-- Файл должен существовать на диске
+					return vim.fn.filereadable(bufname) == 1
+				end,
 			},
 		},
 	},
