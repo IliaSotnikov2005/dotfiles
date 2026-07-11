7cd1e22c08ab5b7d8d8ba069d040f303a4fcf172
Ilya Sotnikov <ilia12st@yandex.ru>
2026-07-11 23:00:33 +0300

    move to noctalia v5

--- niri/.config/niri/scripts/noctalia.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Wrapper: noctalia v5+ uses "noctalia", older uses "qs -c noctalia-shell"
-if command -v noctalia >/dev/null 2>&1; then
-    exec noctalia "$@"
-else
-    exec qs -c noctalia-shell "$@"
-fi
